6d070237c074d00289b700e34c6f41417cbab070
Marcos Leonan <marcosleo014@gmail.com>
2024-12-14 11:54:20 -0300

    palavra arara add ao projeto

--- projeto.py
+++ projeto.py
@@ -2,5 +2,8 @@ from classes import Anagrama
 from funcoes import *
 
 anagrama_nome = Anagrama('marcos')
+#erro ao calcular anagramas dessa palavras, pois tem letras repetidas
+arara = Anagrama('arara')
 
 impressão(anagrama_nome)
+impressão(arara)
